65b874d3f2ed868433141794f843e85bb7205903
OnetakeKG <86379850+OnetakeKG@users.noreply.github.com>
2022-02-18 23:40:42 +0300

    task for lesson 7

--- app/src/main/java/ru/fylmr/poplibs_nov21/di/component/RepoSubcomponent.kt
+++ app/src/main/java/ru/fylmr/poplibs_nov21/di/component/RepoSubcomponent.kt
@@ -1,12 +1,13 @@
 package ru.fylmr.poplibs_nov21.di.component
 
 import dagger.Subcomponent
-import ru.fylmr.poplibs_nov21.di.modules.RepoModule
+
+import ru.fylmr.poplibs_nov21.di.modules.RepositoryModule
 import ru.fylmr.poplibs_nov21.di.scope.RepoScope
 import ru.fylmr.poplibs_nov21.ui.repos.ReposPresenterFactory
 
 @Subcomponent(
-    modules = [RepoModule::class]
+    modules = [RepositoryModule::class]
 )
 @RepoScope
 interface RepoSubcomponent {
